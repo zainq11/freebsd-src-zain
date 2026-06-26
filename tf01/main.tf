f14d142949004b237dab6d8c458b314cd16f914c
Zain Hasan <44829199+zainq11@users.noreply.github.com>
2026-06-26 15:41:55 -0700

    r1_6

--- tf01/main.tf
+++ tf01/main.tf
@@ -2,3 +2,5 @@ resource "null_resource" "r1" {}
 resource "null_resource" "r1_2" {}
 resource "null_resource" "r1_3" {}
 resource "null_resource" "r1_4" {}
+resource "null_resource" "r1_5" {}
+resource "null_resource" "r1_6" {}
